8e39f098627d2efdb3e5dc80336b06cdc6255e58
U-AMR\ndcioban <ndcioban@ndcioban-MOBL1.amr.corp.intel.com>
2015-10-10 10:04:25 -0700

    Added OpenWeatherMap API key
    Signed-off-by: U-AMR\ndcioban <ndcioban@ndcioban-MOBL1.amr.corp.intel.com>

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -147,12 +147,15 @@ public class ForecastFragment extends Fragment {
                 final String FORMAT_PARAM = "mode";
                 final String UNITS_PARAM = "units";
                 final String DAYS_PARAM = "cnt";
+                final String APP_ID = "APPID";
+
 
                 Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
                         .appendQueryParameter(QUERY_PARAM, params[0])
                         .appendQueryParameter(FORMAT_PARAM, format)
                         .appendQueryParameter(UNITS_PARAM, params[1])
                         .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                        .appendQueryParameter(APP_ID, getString(R.string.owm_api_key))
                         .build();
 
                 URL url = new URL(builtUri.toString());
